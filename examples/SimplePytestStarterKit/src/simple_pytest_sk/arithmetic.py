72e7121270eae6656e2912cc32a86d003b1282bb
nicolas <nicolas.tannhaeuser@ibykus.de>
2025-02-26 17:33:16 +0100

    Update Course-Fork

--- examples/SimplePytestStarterKit/src/simple_pytest_sk/arithmetic.py
+++ examples/SimplePytestStarterKit/src/simple_pytest_sk/arithmetic.py
@@ -1,4 +1,12 @@
 def sign(x):
+    '''
+    Computes sign of number
+
+    >>> sign(2)
+    1
+    >>> sign(-2)
+    -1
+    '''
     if x > 0:
         return 1
     if x < 0:
@@ -7,16 +15,46 @@ def sign(x):
 
 
 def lerp(x, y, t):
+    '''
+    Linearly interpolate between two numbers.
+
+    >>> lerp(1, 3, 0)
+    1
+
+    >>> lerp(1, 3, 1)
+    3
+    '''
     return (1 - t) * x + t * y
 
 
 def negate(x):
-    """Return the negative value of x."""
+    """Return the negative value of x.
+    
+    >>> negate(1)
+    -1
+
+    >>> negate(-1)
+    1
+
+    >>> negate(0)
+    0
+        
+    """
     return -x
 
 
 def my_abs(x):
-    """Return the absolute value of x."""
+    """Return the absolute value of x.
+
+    >>> my_abs(2)
+    2
+
+    >>> my_abs(-2)
+    2
+
+    >>> my_abs(0)
+    0
+    """
     if x <= 0:
         return -x
     else:
